e96dcd4ef6cb03f0f9b7649460f3de1a8db0fdde
aravind <aravindraveendran55@gmail.com>
2024-02-17 23:37:35 +0530

    using state to add values in arrays

--- reactbeginner/src/App.jsx
+++ reactbeginner/src/App.jsx
@@ -21,6 +21,7 @@ function App() {
       {/* <UsingMap /> */}
       {/* <Shoppingcart /> */}
 
+      {/* <UsingUseState /> */}
       <UsingUseState />
     </>
   );
--- reactbeginner/src/components/UsingUseState.jsx
+++ reactbeginner/src/components/UsingUseState.jsx
@@ -1,14 +1,41 @@
 import { useState } from "react";
 
 export function UsingUseState() {
-  const [name, setName] = useState("test");
+  const [friends, setFriends] = useState(["arvind", "devika", "asdf"]);
   return (
     <>
-      <h1>{name}</h1>
-      <ChangeNameButton setName={setName} />
+      <h1>My Friends list </h1>
+      {friends.map((f) => (
+        <li key={Math.random()}>{f}</li>
+      ))}
+      <button onClick={() => setFriends([...friends, "Aradhya"])}>
+        Add Friend
+      </button>
+      <button
+        onClick={() => setFriends(friends.filter((fil) => fil !== "Aradhya"))}
+      >
+        Delete Friend
+      </button>
+      <button
+        onClick={() =>
+          setFriends(friends.map((fil) => (fil === "asdf" ? "Aradhya" : fil)))
+        }
+      >
+        Update Friend
+      </button>
     </>
   );
 }
+
+// export function UsingUseState() {
+//   const [name, setName] = useState("test");
+//   return (
+//     <>
+//       <h1>{name}</h1>
+//       <ChangeNameButton setName={setName} />
+//     </>
+//   );
+// }
 
 //normal function
 // function ChangeNameButton({ setName }) {
@@ -16,6 +43,6 @@ export function UsingUseState() {
 // }
 
 //Arrow function
-const ChangeNameButton = ({ setName }) => {
-  return <button onClick={() => setName("devika")}>change me</button>;
-};
+// const ChangeNameButton = ({ setName }) => {
+//   return <button onClick={() => setName("devika")}>change me</button>;
+// };
